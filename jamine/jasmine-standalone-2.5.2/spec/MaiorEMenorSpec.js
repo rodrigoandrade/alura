8a8f62eabb464203cd7d5b1e20ab7c954f3c0ef3
rodrigoandrade <rodrigo.oliveira.and@gmail.com>
2016-10-31 23:27:00 -0200

    Curso Jasmine Alura - Aula 1

---
+++ jamine/jasmine-standalone-2.5.2/spec/MaiorEMenorSpec.js
@@ -0,0 +1,35 @@
+describe("Maior e Menor", function() {
+
+    it("deve entender numeros em ordem nao sequencial", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5,15,7,9]);
+
+        expect(algoritmo.pegaMaior()).toEqual(15);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+    it("deve entender numeros em ordem crescente", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5, 6,7, 8]);
+
+        expect(algoritmo.pegaMaior()).toEqual(8);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+    it("deve entender numeros em ordem decrescente", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([8, 7, 6, 5]);
+
+        expect(algoritmo.pegaMaior()).toEqual(8);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+     it("deve entender apenas um numero na coleção", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([8]);
+
+        expect(algoritmo.pegaMaior()).toEqual(8);
+        expect(algoritmo.pegaMenor()).toEqual(8);
+    });
+
+});
